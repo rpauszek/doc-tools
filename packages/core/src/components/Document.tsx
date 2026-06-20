6635ef17045c9b333195afb5945e0fa8260fda67
Ray Pauszek <ray.pauszek@gmail.com>
2026-06-20 18:11:27 +0200

    core: move in base page styling

--- packages/core/src/components/Document.tsx
+++ packages/core/src/components/Document.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import type { ReactNode } from "react";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import fs from "node:fs"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 type DocumentProps = {
   css: string;
@@ -7,11 +13,16 @@ type DocumentProps = {
 };
 
 export function Document({ css, children }: DocumentProps) {
+
+  const baseCss = path.join(__dirname, "../styles/base.css");
+  const cssBase = fs.readFileSync(baseCss, "utf8");
+  const finalCss = [cssBase, css].filter(Boolean).join("\n");
+
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <style>{css}</style>
+        <style>{finalCss}</style>
       </head>
       <body>{children}</body>
     </html>
